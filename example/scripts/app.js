aad8f45cb4cd3e3176a6c7b9fe4d2b92585d4554
fraserxu <xvfeng123@gmail.com>
2013-10-12 20:07:49 +0800

    update core file with own style

--- example/scripts/app.js
+++ example/scripts/app.js
@@ -1,5 +1,5 @@
 angular.module('textcompleteApp', ['ngTextcomplete'])
 
 .controller('textcompleteCtrl', ['$scope', function($scope) {
-  $scope.members = ['fraserxu', 'github', 'ngTextcomplete'];
+  $scope.members = ['fraserxu/13', 'github/23', 'ngTextcomplete/23', 'github/32', 'github/12', 'github/45'];
 }]);
